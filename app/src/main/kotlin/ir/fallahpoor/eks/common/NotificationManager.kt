0ed44df3157fd704bda30f12993cdcc3ba52bbf5
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2022-11-12 15:08:23 +0100

    Fix an issue reported by Lint

--- app/src/main/kotlin/ir/fallahpoor/eks/common/NotificationManager.kt
+++ app/src/main/kotlin/ir/fallahpoor/eks/common/NotificationManager.kt
@@ -4,6 +4,7 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -67,7 +68,7 @@ class NotificationManager
         val intent = Intent(context, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
-        return PendingIntent.getActivity(context, 0, intent, 0)
+        return PendingIntent.getActivity(context, 0, intent, FLAG_IMMUTABLE)
     }
 
 }
