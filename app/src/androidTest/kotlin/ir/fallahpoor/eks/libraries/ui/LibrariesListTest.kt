a3227166b2e3166add56a04e097811c07ad65230
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2022-11-17 15:39:24 +0100

    Add a button to scroll to the top of the libraries list

--- app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/LibrariesListTest.kt
+++ app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/LibrariesListTest.kt
@@ -2,9 +2,12 @@ package ir.fallahpoor.eks.libraries.ui
 
 import android.content.Context
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToIndex
 import androidx.test.core.app.ApplicationProvider
 import ir.fallahpoor.eks.R
 import ir.fallahpoor.eks.commontest.TestData
@@ -22,47 +25,134 @@ class LibrariesListTest {
     private val context: Context = ApplicationProvider.getApplicationContext()
 
     @Test
-    fun list_of_libraries_is_displayed() {
+    fun when_there_are_libraries_the_list_of_libraries_is_displayed() {
 
         // Given
-        val libraries = listOf(
-            TestData.core,
-            TestData.room,
-            TestData.preference
-        )
-
-        // When
-        composeLibrariesList(libraries = libraries)
+        composeLibrariesList(libraries = getListOfLibraries())
 
         // Then
         with(composeRule) {
-            libraries.forEach {
-                onNodeWithTag(
-                    LibraryItemTags.ITEM + it.name,
-                    useUnmergedTree = true
-                ).assertIsDisplayed()
-            }
-            onNodeWithText(context.getString(R.string.no_library), useUnmergedTree = true)
-                .assertDoesNotExist()
+            onNodeWithTag(
+                LibrariesListTags.LIBRARIES_LIST,
+                useUnmergedTree = true
+            ).assertIsDisplayed()
+            onNodeWithText(
+                context.getString(R.string.no_library),
+                useUnmergedTree = true
+            ).assertDoesNotExist()
+        }
+
+    }
+
+    private fun getListOfLibraries(): List<Library> = mutableListOf<Library>().apply {
+        repeat(30) {
+            this += TestData.core.copy(name = "core$it")
+        }
+    }
+
+    @Test
+    fun when_there_are_libraries_no_libraries_message_is_not_displayed() {
+
+        // Given
+        composeLibrariesList(libraries = getListOfLibraries())
+
+        // Then
+        with(composeRule) {
+            onNodeWithText(
+                context.getString(R.string.no_library),
+                useUnmergedTree = true
+            ).assertDoesNotExist()
         }
 
     }
 
     @Test
-    fun a_proper_message_is_displayed_when_there_is_no_library() {
+    fun when_at_the_top_of_list_of_libraries_scroll_to_top_button_is_not_displayed() {
+
+        // Given
+        val libraries = getListOfLibraries()
+        composeLibrariesList(libraries = libraries)
+        composeRule.onNodeWithTag(LibrariesListTags.LIBRARIES_LIST)
+            .performScrollToIndex(libraries.lastIndex)
 
         // When
-        val testTag = "libraries"
-        composeLibrariesList(modifier = Modifier.testTag(testTag), libraries = emptyList())
+        composeRule.onNodeWithTag(LibrariesListTags.LIBRARIES_LIST).performScrollToIndex(0)
 
         // Then
-        with(composeRule) {
-            onNodeWithTag(testTag)
-                .onChildren()
-                .assertCountEquals(1)
-            onNodeWithText(context.getString(R.string.no_library), useUnmergedTree = true)
-                .assertIsDisplayed()
-        }
+        composeRule.onNodeWithTag(LibrariesListTags.SCROLL_TO_TOP_BUTTON, useUnmergedTree = true)
+            .assertDoesNotExist()
+
+    }
+
+    @Test
+    fun when_not_at_the_top_of_the_list_of_libraries_scroll_to_top_button_is_displayed() {
+
+        // Given
+        val libraries = getListOfLibraries()
+        composeLibrariesList(libraries = libraries)
+
+        // When
+        composeRule.onNodeWithTag(LibrariesListTags.LIBRARIES_LIST)
+            .performScrollToIndex(libraries.lastIndex)
+
+        // Then
+        composeRule.onNodeWithTag(LibrariesListTags.SCROLL_TO_TOP_BUTTON, useUnmergedTree = true)
+            .assertIsDisplayed()
+
+    }
+
+    @Test
+    fun when_clicking_the_scroll_to_top_button_the_list_of_libraries_is_scrolled_to_the_top() {
+
+        // Given
+        val libraries = getListOfLibraries().toMutableList()
+        libraries.add(0, TestData.room)
+        composeLibrariesList(libraries = libraries)
+        composeRule.onNodeWithTag(LibrariesListTags.LIBRARIES_LIST)
+            .performScrollToIndex(libraries.lastIndex)
+
+        // When
+        composeRule.onNodeWithTag(LibrariesListTags.SCROLL_TO_TOP_BUTTON).performClick()
+
+        // Then
+        composeRule.onNodeWithTag(LibraryItemTags.ITEM + TestData.room.name, useUnmergedTree = true)
+            .assertIsDisplayed()
+
+    }
+
+    @Test
+    fun when_there_are_no_libraries_no_libraries_message_is_displayed() {
+
+        // Given
+        composeLibrariesList(libraries = emptyList())
+
+        // Then
+        composeRule.onNodeWithText(context.getString(R.string.no_library), useUnmergedTree = true)
+            .assertIsDisplayed()
+
+    }
+
+    @Test
+    fun when_there_are_no_libraries_list_of_libraries_is_not_displayed() {
+
+        // Given
+        composeLibrariesList(libraries = emptyList())
+
+        // Then
+        composeRule.onNodeWithTag(LibrariesListTags.LIBRARIES_LIST, useUnmergedTree = true)
+            .assertDoesNotExist()
+
+    }
+
+    @Test
+    fun when_there_are_no_libraries_scroll_to_top_button_is_not_displayed() {
+
+        // Given
+        composeLibrariesList(libraries = emptyList())
+
+        // Then
+        composeRule.onNodeWithTag(LibrariesListTags.SCROLL_TO_TOP_BUTTON, useUnmergedTree = true)
+            .assertDoesNotExist()
 
     }
 
@@ -73,16 +163,35 @@ class LibrariesListTest {
         val library: Library = TestData.room
         val onLibraryClick: (Library) -> Unit = mock()
         composeLibrariesList(
-            libraries = listOf(library),
-            onLibraryClick = onLibraryClick
+            libraries = listOf(library), onLibraryClick = onLibraryClick
         )
 
         // When
-        composeRule.onNodeWithTag(LibraryItemTags.ITEM + library.name)
-            .performClick()
+        composeRule.onNodeWithTag(LibraryItemTags.ITEM + library.name).performClick()
 
         // Then
         Mockito.verify(onLibraryClick).invoke(library)
+
+    }
+
+    @Test
+    fun correct_callback_is_called_when_a_library_version_is_clicked() {
+
+        // Given
+        val library: Library = TestData.room
+        val onLibraryVersionClick: (Version) -> Unit = mock()
+        composeLibrariesList(
+            libraries = listOf(library), onLibraryVersionClick = onLibraryVersionClick
+        )
+
+        // When
+        composeRule.onNodeWithText(
+            context.getString(R.string.version_stable, library.stableVersion.name),
+            useUnmergedTree = true
+        ).performClick()
+
+        // Then
+        Mockito.verify(onLibraryVersionClick).invoke(library.stableVersion)
 
     }
 
@@ -93,13 +202,11 @@ class LibrariesListTest {
         val library: Library = TestData.preference
         val onLibraryPinClick: (Library, Boolean) -> Unit = mock()
         composeLibrariesList(
-            libraries = listOf(library),
-            onLibraryPinClick = onLibraryPinClick
+            libraries = listOf(library), onLibraryPinClick = onLibraryPinClick
         )
 
         // When
-        composeRule.onNodeWithTag(LibraryItemTags.PIN_BUTTON + library.name)
-            .performClick()
+        composeRule.onNodeWithTag(LibraryItemTags.PIN_BUTTON + library.name).performClick()
 
         // Then
         Mockito.verify(onLibraryPinClick).invoke(library, true)
@@ -113,13 +220,11 @@ class LibrariesListTest {
         val library: Library = TestData.core
         val onLibraryPinClick: (Library, Boolean) -> Unit = mock()
         composeLibrariesList(
-            libraries = listOf(library),
-            onLibraryPinClick = onLibraryPinClick
+            libraries = listOf(library), onLibraryPinClick = onLibraryPinClick
         )
 
         // When
-        composeRule.onNodeWithTag(LibraryItemTags.PIN_BUTTON + library.name)
-            .performClick()
+        composeRule.onNodeWithTag(LibraryItemTags.PIN_BUTTON + library.name).performClick()
 
         // Then
         Mockito.verify(onLibraryPinClick).invoke(library, false)
--- app/src/main/kotlin/ir/fallahpoor/eks/libraries/ui/LibrariesList.kt
+++ app/src/main/kotlin/ir/fallahpoor/eks/libraries/ui/LibrariesList.kt
@@ -4,6 +4,7 @@ package ir.fallahpoor.eks.libraries.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Divider
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
@@ -23,10 +33,12 @@ import ir.fallahpoor.eks.R
 import ir.fallahpoor.eks.data.entity.Library
 import ir.fallahpoor.eks.data.entity.Version
 import ir.fallahpoor.eks.theme.spacing
+import kotlinx.coroutines.launch
 
 object LibrariesListTags {
     const val LIBRARIES_LIST = "librariesListLibrariesList"
-    const val NO_LIBRARY_TEXT = "librariesListNoLibrary"
+    const val NO_LIBRARY_TEXT = "librariesListNoLibraryText"
+    const val SCROLL_TO_TOP_BUTTON = "librariesListScrollToTopButton"
 }
 
 @Composable
@@ -37,39 +49,56 @@ fun LibrariesList(
     onLibraryVersionClick: (Version) -> Unit,
     onLibraryPinClick: (Library, Boolean) -> Unit
 ) {
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.BottomStart
-    ) {
+    Box(modifier = modifier) {
         AnimatedVisibility(
             visible = libraries.isEmpty(),
-            enter = fadeIn()
+            enter = fadeIn(),
+            exit = fadeOut()
         ) {
             NoLibrary()
         }
         AnimatedVisibility(
             visible = libraries.isNotEmpty(),
-            enter = fadeIn()
+            enter = fadeIn(),
+            exit = fadeOut()
         ) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .testTag(LibrariesListTags.LIBRARIES_LIST)
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.BottomEnd
             ) {
-                items(
-                    items = libraries,
-                    key = { library: Library -> library.name }
-                ) { library: Library ->
-                    LibraryItem(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .animateItemPlacement(),
-                        library = library,
-                        onLibraryClick = onLibraryClick,
-                        onLibraryVersionClick = onLibraryVersionClick,
-                        onLibraryPinClick = onLibraryPinClick
-                    )
-                    Divider()
+                val listState = rememberLazyListState()
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .testTag(LibrariesListTags.LIBRARIES_LIST),
+                    state = listState
+                ) {
+                    items(
+                        items = libraries,
+                        key = { library: Library -> library.name }
+                    ) { library: Library ->
+                        LibraryItem(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .animateItemPlacement(),
+                            library = library,
+                            onLibraryClick = onLibraryClick,
+                            onLibraryVersionClick = onLibraryVersionClick,
+                            onLibraryPinClick = onLibraryPinClick
+                        )
+                        Divider()
+                    }
+                }
+                val coroutineScope = rememberCoroutineScope()
+                val showScrollToTopButton by remember {
+                    derivedStateOf {
+                        listState.firstVisibleItemIndex > 0
+                    }
+                }
+                ScrollToTopButton(show = showScrollToTopButton) {
+                    coroutineScope.launch {
+                        listState.animateScrollToItem(0)
+                    }
                 }
             }
         }
@@ -88,5 +117,26 @@ private fun NoLibrary() {
             modifier = Modifier.padding(MaterialTheme.spacing.normal),
             text = stringResource(R.string.no_library)
         )
+    }
+}
+
+@Composable
+private fun ScrollToTopButton(show: Boolean, onClick: () -> Unit) {
+    AnimatedVisibility(
+        visible = show,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        FloatingActionButton(
+            modifier = Modifier
+                .padding(MaterialTheme.spacing.normal)
+                .testTag(LibrariesListTags.SCROLL_TO_TOP_BUTTON),
+            onClick = onClick
+        ) {
+            Icon(
+                imageVector = Icons.Filled.KeyboardArrowUp,
+                contentDescription = stringResource(R.string.scroll_to_top)
+            )
+        }
     }
 }
