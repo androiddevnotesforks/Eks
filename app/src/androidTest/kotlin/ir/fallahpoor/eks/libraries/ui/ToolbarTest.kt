0404d502cb2d5ef1a08d103e6f7c6f7dd60aa230
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2022-11-24 14:48:40 +0100

    Refactor ToolbarTest

--- app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/ToolbarTest.kt
+++ app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/ToolbarTest.kt
@@ -2,11 +2,10 @@ package ir.fallahpoor.eks.libraries.ui
 
 import android.content.Context
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performTextInput
 import androidx.test.core.app.ApplicationProvider
 import ir.fallahpoor.eks.R
 import ir.fallahpoor.eks.data.SortOrder
+import ir.fallahpoor.eks.libraries.ui.robots.ToolbarRobot
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
@@ -17,23 +16,18 @@ class ToolbarTest {
     val composeTestRule = createComposeRule()
 
     private val context: Context = ApplicationProvider.getApplicationContext()
+    private val toolbarRobot = ToolbarRobot(context, composeTestRule)
     private val sortText = context.getString(R.string.sort)
-    private val searchText = context.getString(R.string.search)
     private val selectSortOrderText = context.getString(R.string.select_sort_order)
 
     @Test
     fun toolbar_is_initialized_correctly() {
 
         // Given
-        composeToolbar()
+        toolbarRobot.composeToolbar()
 
         // Then
-        with(composeTestRule) {
-            assertTextIsDisplayed(context.getString(R.string.app_name))
-            assertIsDisplayedNodeWithContentDescription(sortText)
-            assertIsDisplayedNodeWithContentDescription(searchText)
-            assertDoesNotExistNodeWithTag(SearchBarTags.SEARCH_BAR)
-        }
+        toolbarRobot.assertElementsAreDisplayedCorrectly()
 
     }
 
@@ -41,7 +35,7 @@ class ToolbarTest {
     fun when_sort_button_is_clicked_sort_dialog_is_displayed() {
 
         // Given
-        composeToolbar()
+        toolbarRobot.composeToolbar()
 
         // When
         composeTestRule.clickOnNodeWithContentDescription(sortText)
@@ -55,10 +49,10 @@ class ToolbarTest {
     fun when_search_button_is_clicked_search_bar_is_displayed() {
 
         // Given
-        composeToolbar()
+        toolbarRobot.composeToolbar()
 
         // When
-        clickOnSearchButton()
+        toolbarRobot.clickOnSearchButton()
 
         // Then
         composeTestRule.assertIsDisplayedNodeWithTag(SearchBarTags.SEARCH_BAR)
@@ -70,29 +64,24 @@ class ToolbarTest {
 
         // Given
         val onSortOrderChange: (SortOrder) -> Unit = mock()
-        composeToolbar(onSortOrderChange = onSortOrderChange)
+        toolbarRobot.composeToolbar(onSortOrderChange = onSortOrderChange)
 
         // When
-        selectSortOrder(SortOrder.Z_TO_A)
+        toolbarRobot.selectSortOrder(SortOrder.Z_TO_A)
 
         // Then
         Mockito.verify(onSortOrderChange).invoke(SortOrder.Z_TO_A)
 
     }
 
-    private fun selectSortOrder(sortOrder: SortOrder) {
-        composeTestRule.clickOnNodeWithContentDescription(sortText)
-        composeTestRule.clickOnNodeWithText(context.getString(sortOrder.stringResId))
-    }
-
     @Test
     fun when_sort_order_is_selected_sort_order_dialog_is_closed() {
 
         // Given
-        composeToolbar()
+        toolbarRobot.composeToolbar()
 
         // When
-        selectSortOrder(SortOrder.A_TO_Z)
+        toolbarRobot.selectSortOrder(SortOrder.A_TO_Z)
 
         // Then
         composeTestRule.assertTextDoesNotExist(selectSortOrderText)
@@ -104,20 +93,14 @@ class ToolbarTest {
 
         // Given
         val onSearchQueryChange: (String) -> Unit = mock()
-        composeToolbar(onSearchQueryChange = onSearchQueryChange)
+        toolbarRobot.composeToolbar(onSearchQueryChange = onSearchQueryChange)
 
         // When
-        enterSearchQuery("Coil")
+        toolbarRobot.enterSearchQuery("Coil")
 
         // Then
         Mockito.verify(onSearchQueryChange).invoke("Coil")
 
-    }
-
-    private fun enterSearchQuery(searchQuery: String) {
-        clickOnSearchButton()
-        composeTestRule.onNodeWithTag(SearchBarTags.QUERY_TEXT_FIELD)
-            .performTextInput(searchQuery)
     }
 
     @Test
@@ -125,10 +108,10 @@ class ToolbarTest {
 
         // Given
         val onSearchQueryChange: (String) -> Unit = mock()
-        composeToolbar(searchQuery = "Koin", onSearchQueryChange = onSearchQueryChange)
+        toolbarRobot.composeToolbar(searchQuery = "Koin", onSearchQueryChange = onSearchQueryChange)
 
         // When
-        clearSearchQuery()
+        toolbarRobot.clearSearchQuery()
 
         // Then
         Mockito.verify(onSearchQueryChange).invoke("")
@@ -139,46 +122,14 @@ class ToolbarTest {
     fun when_search_bar_is_closed_toolbar_is_set_to_normal_mode() {
 
         // Given
-        composeToolbar()
+        toolbarRobot.composeToolbar()
 
         // When
-        closeSearchBar()
+        toolbarRobot.closeSearchBar()
 
         // Then
         composeTestRule.assertDoesNotExistNodeWithTag(SearchBarTags.SEARCH_BAR)
 
-    }
-
-    private fun clearSearchQuery() {
-        clickOnSearchButton()
-        composeTestRule.clickOnNodeWithTag(SearchBarTags.CLEAR_BUTTON)
-    }
-
-    private fun closeSearchBar() {
-        composeTestRule.clickOnNodeWithContentDescription(searchText)
-        composeTestRule.clickOnNodeWithTag(SearchBarTags.CLOSE_BUTTON)
-    }
-
-    private fun clickOnSearchButton() {
-        composeTestRule.clickOnNodeWithContentDescription(searchText)
-    }
-
-    private fun composeToolbar(
-        sortOrder: SortOrder = SortOrder.A_TO_Z,
-        onSortOrderChange: (SortOrder) -> Unit = {},
-        searchQuery: String = "",
-        onSearchQueryChange: (String) -> Unit = {},
-        onSearchQuerySubmit: (String) -> Unit = {}
-    ) {
-        composeTestRule.setContent {
-            Toolbar(
-                sortOrder = sortOrder,
-                onSortOrderChange = onSortOrderChange,
-                searchQuery = searchQuery,
-                onSearchQueryChange = onSearchQueryChange,
-                onSearchQuerySubmit = onSearchQuerySubmit
-            )
-        }
     }
 
 }
---
+++ app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/robots/ToolbarRobot.kt
@@ -0,0 +1,78 @@
+package ir.fallahpoor.eks.libraries.ui.robots
+
+import android.content.Context
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performTextInput
+import ir.fallahpoor.eks.R
+import ir.fallahpoor.eks.data.SortOrder
+import ir.fallahpoor.eks.libraries.ui.SearchBarTags
+import ir.fallahpoor.eks.libraries.ui.Toolbar
+import ir.fallahpoor.eks.libraries.ui.assertDoesNotExistNodeWithTag
+import ir.fallahpoor.eks.libraries.ui.assertIsDisplayedNodeWithContentDescription
+import ir.fallahpoor.eks.libraries.ui.assertTextIsDisplayed
+import ir.fallahpoor.eks.libraries.ui.clickOnNodeWithContentDescription
+import ir.fallahpoor.eks.libraries.ui.clickOnNodeWithTag
+import ir.fallahpoor.eks.libraries.ui.clickOnNodeWithText
+
+class ToolbarRobot(
+    private val context: Context,
+    private val composeTestRule: ComposeContentTestRule
+) {
+
+    private val sortText = context.getString(R.string.sort)
+    private val searchText = context.getString(R.string.search)
+
+    fun assertElementsAreDisplayedCorrectly() {
+        with(composeTestRule) {
+            assertTextIsDisplayed(context.getString(R.string.app_name))
+            assertIsDisplayedNodeWithContentDescription(sortText)
+            assertIsDisplayedNodeWithContentDescription(searchText)
+            assertDoesNotExistNodeWithTag(SearchBarTags.SEARCH_BAR)
+        }
+    }
+
+    fun composeToolbar(
+        sortOrder: SortOrder = SortOrder.A_TO_Z,
+        onSortOrderChange: (SortOrder) -> Unit = {},
+        searchQuery: String = "",
+        onSearchQueryChange: (String) -> Unit = {},
+        onSearchQuerySubmit: (String) -> Unit = {}
+    ) {
+        composeTestRule.setContent {
+            Toolbar(
+                sortOrder = sortOrder,
+                onSortOrderChange = onSortOrderChange,
+                searchQuery = searchQuery,
+                onSearchQueryChange = onSearchQueryChange,
+                onSearchQuerySubmit = onSearchQuerySubmit
+            )
+        }
+    }
+
+    fun clearSearchQuery() {
+        clickOnSearchButton()
+        composeTestRule.clickOnNodeWithTag(SearchBarTags.CLEAR_BUTTON)
+    }
+
+    fun closeSearchBar() {
+        composeTestRule.clickOnNodeWithContentDescription(searchText)
+        composeTestRule.clickOnNodeWithTag(SearchBarTags.CLOSE_BUTTON)
+    }
+
+    fun clickOnSearchButton() {
+        composeTestRule.clickOnNodeWithContentDescription(searchText)
+    }
+
+    fun selectSortOrder(sortOrder: SortOrder) {
+        composeTestRule.clickOnNodeWithContentDescription(sortText)
+        composeTestRule.clickOnNodeWithText(context.getString(sortOrder.stringResId))
+    }
+
+    fun enterSearchQuery(searchQuery: String) {
+        clickOnSearchButton()
+        composeTestRule.onNodeWithTag(SearchBarTags.QUERY_TEXT_FIELD)
+            .performTextInput(searchQuery)
+    }
+
+}
