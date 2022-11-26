b0dc6fcb1a1aaa6a1942e2df54360e530cdfb647
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2022-11-26 13:59:21 +0100

    Refactor LibrariesContentTest

--- app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/LibrariesContentTest.kt
+++ app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/LibrariesContentTest.kt
@@ -3,10 +3,10 @@ package ir.fallahpoor.eks.libraries.ui
 import android.content.Context
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.core.app.ApplicationProvider
-import ir.fallahpoor.eks.R
 import ir.fallahpoor.eks.commontest.TestData
 import ir.fallahpoor.eks.data.entity.Library
 import ir.fallahpoor.eks.data.entity.Version
+import ir.fallahpoor.eks.libraries.ui.robots.LibrariesContentRobot
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
@@ -17,15 +17,16 @@ class LibrariesContentTest {
     val composeRule = createComposeRule()
 
     private val context: Context = ApplicationProvider.getApplicationContext()
+    private val librariesContentRobot = LibrariesContentRobot(context, composeRule)
 
     @Test
     fun when_state_is_LOADING_progress_indicator_is_displayed() {
 
         // Given
-        composeLibrariesContent(librariesState = LibrariesState.Loading)
+        librariesContentRobot.composeLibrariesContent(librariesState = LibrariesState.Loading)
 
         // Then
-        composeRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.PROGRESS_INDICATOR)
+        librariesContentRobot.progressIndicatorIsDisplayed()
 
     }
 
@@ -33,20 +34,21 @@ class LibrariesContentTest {
     fun when_state_is_LOADING_refresh_date_is_not_displayed() {
 
         // Given
-        composeLibrariesContent(librariesState = LibrariesState.Loading)
+        librariesContentRobot.composeLibrariesContent(librariesState = LibrariesState.Loading)
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.REFRESH_DATE)
+        librariesContentRobot.refreshDateIsNotDisplayed()
+
     }
 
     @Test
     fun when_state_is_LOADING_libraries_are_not_displayed() {
 
         // Given
-        composeLibrariesContent(librariesState = LibrariesState.Loading)
+        librariesContentRobot.composeLibrariesContent(librariesState = LibrariesState.Loading)
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.LIBRARIES_LIST)
+        librariesContentRobot.librariesListIsNotDisplayed()
 
     }
 
@@ -54,10 +56,10 @@ class LibrariesContentTest {
     fun when_state_is_LOADING_try_again_is_not_displayed() {
 
         // Given
-        composeLibrariesContent(librariesState = LibrariesState.Loading)
+        librariesContentRobot.composeLibrariesContent(librariesState = LibrariesState.Loading)
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.TRY_AGAIN_LAYOUT)
+        librariesContentRobot.tryAgainIsNotDisplayed()
 
     }
 
@@ -70,10 +72,14 @@ class LibrariesContentTest {
             TestData.Biometric.old,
             TestData.core, TestData.preference
         )
-        composeLibrariesContent(librariesState = LibrariesState.Success(libraries))
+        librariesContentRobot.composeLibrariesContent(
+            librariesState = LibrariesState.Success(
+                libraries
+            )
+        )
 
         // Then
-        composeRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.LIBRARIES_LIST)
+        librariesContentRobot.librariesListIsDisplayed()
 
     }
 
@@ -87,10 +93,14 @@ class LibrariesContentTest {
             TestData.core,
             TestData.preference
         )
-        composeLibrariesContent(librariesState = LibrariesState.Success(libraries))
+        librariesContentRobot.composeLibrariesContent(
+            librariesState = LibrariesState.Success(
+                libraries
+            )
+        )
 
         // Then
-        composeRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.REFRESH_DATE)
+        librariesContentRobot.refreshDateIsDisplayed()
 
     }
 
@@ -104,10 +114,14 @@ class LibrariesContentTest {
             TestData.core,
             TestData.preference
         )
-        composeLibrariesContent(librariesState = LibrariesState.Success(libraries))
+        librariesContentRobot.composeLibrariesContent(
+            librariesState = LibrariesState.Success(
+                libraries
+            )
+        )
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.PROGRESS_INDICATOR)
+        librariesContentRobot.progressIndicatorIsNotDisplayed()
 
     }
 
@@ -121,10 +135,14 @@ class LibrariesContentTest {
             TestData.core,
             TestData.preference
         )
-        composeLibrariesContent(librariesState = LibrariesState.Success(libraries))
+        librariesContentRobot.composeLibrariesContent(
+            librariesState = LibrariesState.Success(
+                libraries
+            )
+        )
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.TRY_AGAIN_LAYOUT)
+        librariesContentRobot.tryAgainIsNotDisplayed()
 
     }
 
@@ -133,11 +151,15 @@ class LibrariesContentTest {
 
         // Given
         val errorMessage = "An error occurred."
-        composeLibrariesContent(librariesState = LibrariesState.Error(errorMessage))
+        librariesContentRobot.composeLibrariesContent(
+            librariesState = LibrariesState.Error(
+                errorMessage
+            )
+        )
 
         // Then
-        composeRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.TRY_AGAIN_LAYOUT)
-        composeRule.assertTextIsDisplayed(errorMessage)
+        librariesContentRobot.tryAgainIsDisplayed()
+        librariesContentRobot.errorMessageIsDisplayed(errorMessage)
 
     }
 
@@ -145,10 +167,10 @@ class LibrariesContentTest {
     fun when_state_is_ERROR_libraries_are_not_displayed() {
 
         // Given
-        composeLibrariesContent(librariesState = LibrariesState.Error("An error occurred."))
+        librariesContentRobot.composeLibrariesContent(librariesState = LibrariesState.Error("An error occurred."))
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.LIBRARIES_LIST)
+        librariesContentRobot.librariesListIsNotDisplayed()
 
     }
 
@@ -156,10 +178,10 @@ class LibrariesContentTest {
     fun when_state_is_ERROR_refresh_date_is_not_displayed() {
 
         // Given
-        composeLibrariesContent(librariesState = LibrariesState.Error("An error occurred."))
+        librariesContentRobot.composeLibrariesContent(librariesState = LibrariesState.Error("An error occurred."))
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.REFRESH_DATE)
+        librariesContentRobot.refreshDateIsNotDisplayed()
 
     }
 
@@ -167,10 +189,10 @@ class LibrariesContentTest {
     fun when_state_is_ERROR_progress_indicator_is_not_displayed() {
 
         // Given
-        composeLibrariesContent(librariesState = LibrariesState.Error("An error occurred."))
+        librariesContentRobot.composeLibrariesContent(librariesState = LibrariesState.Error("An error occurred."))
 
         // Then
-        composeRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.PROGRESS_INDICATOR)
+        librariesContentRobot.progressIndicatorIsNotDisplayed()
 
     }
 
@@ -180,13 +202,13 @@ class LibrariesContentTest {
         // Given
         val library: Library = TestData.room
         val onLibraryClick: (Library) -> Unit = mock()
-        composeLibrariesContent(
+        librariesContentRobot.composeLibrariesContent(
             librariesState = LibrariesState.Success(listOf(library)),
             onLibraryClick = onLibraryClick
         )
 
         // When
-        composeRule.clickOnNodeWithTag(LibraryItemTags.ITEM + library.name)
+        librariesContentRobot.clickOnLibrary(library)
 
         // Then
         Mockito.verify(onLibraryClick).invoke(library)
@@ -199,15 +221,13 @@ class LibrariesContentTest {
         // Given
         val library: Library = TestData.room
         val onLibraryVersionClick: (Version) -> Unit = mock()
-        composeLibrariesContent(
+        librariesContentRobot.composeLibrariesContent(
             librariesState = LibrariesState.Success(listOf(library)),
             onLibraryVersionClick = onLibraryVersionClick
         )
 
         // When
-        composeRule.clickOnNodeWithText(
-            context.getString(R.string.version_stable, library.stableVersion.name)
-        )
+        librariesContentRobot.clickOnLibraryStableVersion(library)
 
         // Then
         Mockito.verify(onLibraryVersionClick).invoke(library.stableVersion)
@@ -220,13 +240,13 @@ class LibrariesContentTest {
         // Given
         val library: Library = TestData.room
         val onLibraryPinClick: (Library, Boolean) -> Unit = mock()
-        composeLibrariesContent(
+        librariesContentRobot.composeLibrariesContent(
             librariesState = LibrariesState.Success(listOf(library)),
             onLibraryPinClick = onLibraryPinClick
         )
 
         // When
-        composeRule.clickOnNodeWithTag(LibraryItemTags.PIN_BUTTON + library.name)
+        librariesContentRobot.clickOnPin(library)
 
         // Then
         Mockito.verify(onLibraryPinClick).invoke(library, true)
@@ -239,13 +259,13 @@ class LibrariesContentTest {
         // Given
         val library: Library = TestData.core
         val onLibraryPinClick: (Library, Boolean) -> Unit = mock()
-        composeLibrariesContent(
+        librariesContentRobot.composeLibrariesContent(
             librariesState = LibrariesState.Success(listOf(library)),
             onLibraryPinClick = onLibraryPinClick
         )
 
         // When
-        composeRule.clickOnNodeWithTag(LibraryItemTags.PIN_BUTTON + library.name)
+        librariesContentRobot.clickOnPin(library)
 
         // Then
         Mockito.verify(onLibraryPinClick).invoke(library, false)
@@ -257,37 +277,17 @@ class LibrariesContentTest {
 
         // Given
         val onTryAgainClick: () -> Unit = mock()
-        composeLibrariesContent(
+        librariesContentRobot.composeLibrariesContent(
             librariesState = LibrariesState.Error("something went wrong"),
             onTryAgainClick = onTryAgainClick
         )
 
         // When
-        composeRule.clickOnNodeWithTag(LibrariesContentTags.TRY_AGAIN_BUTTON)
+        librariesContentRobot.clickOnTryAgain()
 
         // Then
         Mockito.verify(onTryAgainClick).invoke()
 
-    }
-
-    private fun composeLibrariesContent(
-        librariesState: LibrariesState = LibrariesState.Loading,
-        refreshDate: String = "N/A",
-        onLibraryClick: (Library) -> Unit = {},
-        onLibraryVersionClick: (Version) -> Unit = {},
-        onLibraryPinClick: (Library, Boolean) -> Unit = { _, _ -> },
-        onTryAgainClick: () -> Unit = {}
-    ) {
-        composeRule.setContent {
-            LibrariesContent(
-                librariesState = librariesState,
-                refreshDate = refreshDate,
-                onLibraryClick = onLibraryClick,
-                onLibraryVersionClick = onLibraryVersionClick,
-                onLibraryPinClick = onLibraryPinClick,
-                onTryAgainClick = onTryAgainClick
-            )
-        }
     }
 
 }
---
+++ app/src/androidTest/kotlin/ir/fallahpoor/eks/libraries/ui/robots/LibrariesContentRobot.kt
@@ -0,0 +1,97 @@
+package ir.fallahpoor.eks.libraries.ui.robots
+
+import android.content.Context
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import ir.fallahpoor.eks.R
+import ir.fallahpoor.eks.data.entity.Library
+import ir.fallahpoor.eks.data.entity.Version
+import ir.fallahpoor.eks.libraries.ui.LibrariesContent
+import ir.fallahpoor.eks.libraries.ui.LibrariesContentTags
+import ir.fallahpoor.eks.libraries.ui.LibrariesState
+import ir.fallahpoor.eks.libraries.ui.LibraryItemTags
+import ir.fallahpoor.eks.libraries.ui.assertDoesNotExistNodeWithTag
+import ir.fallahpoor.eks.libraries.ui.assertIsDisplayedNodeWithTag
+import ir.fallahpoor.eks.libraries.ui.assertTextIsDisplayed
+import ir.fallahpoor.eks.libraries.ui.clickOnNodeWithTag
+import ir.fallahpoor.eks.libraries.ui.clickOnNodeWithText
+
+class LibrariesContentRobot(
+    private val context: Context,
+    private val composeTestRule: ComposeContentTestRule
+) {
+
+    fun composeLibrariesContent(
+        librariesState: LibrariesState = LibrariesState.Loading,
+        refreshDate: String = "N/A",
+        onLibraryClick: (Library) -> Unit = {},
+        onLibraryVersionClick: (Version) -> Unit = {},
+        onLibraryPinClick: (Library, Boolean) -> Unit = { _, _ -> },
+        onTryAgainClick: () -> Unit = {}
+    ) {
+        composeTestRule.setContent {
+            LibrariesContent(
+                librariesState = librariesState,
+                refreshDate = refreshDate,
+                onLibraryClick = onLibraryClick,
+                onLibraryVersionClick = onLibraryVersionClick,
+                onLibraryPinClick = onLibraryPinClick,
+                onTryAgainClick = onTryAgainClick
+            )
+        }
+    }
+
+    fun clickOnLibrary(library: Library) {
+        composeTestRule.clickOnNodeWithTag(LibraryItemTags.ITEM + library.name)
+    }
+
+    fun clickOnLibraryStableVersion(library: Library) {
+        composeTestRule.clickOnNodeWithText(
+            context.getString(R.string.version_stable, library.stableVersion.name)
+        )
+    }
+
+    fun clickOnPin(library: Library) {
+        composeTestRule.clickOnNodeWithTag(LibraryItemTags.PIN_BUTTON + library.name)
+    }
+
+    fun clickOnTryAgain() {
+        composeTestRule.clickOnNodeWithTag(LibrariesContentTags.TRY_AGAIN_BUTTON)
+    }
+
+    fun progressIndicatorIsDisplayed() {
+        composeTestRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.PROGRESS_INDICATOR)
+    }
+
+    fun progressIndicatorIsNotDisplayed() {
+        composeTestRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.PROGRESS_INDICATOR)
+    }
+
+    fun refreshDateIsDisplayed() {
+        composeTestRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.REFRESH_DATE)
+    }
+
+    fun refreshDateIsNotDisplayed() {
+        composeTestRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.REFRESH_DATE)
+    }
+
+    fun librariesListIsDisplayed() {
+        composeTestRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.LIBRARIES_LIST)
+    }
+
+    fun librariesListIsNotDisplayed() {
+        composeTestRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.LIBRARIES_LIST)
+    }
+
+    fun tryAgainIsDisplayed() {
+        composeTestRule.assertIsDisplayedNodeWithTag(LibrariesContentTags.TRY_AGAIN_LAYOUT)
+    }
+
+    fun tryAgainIsNotDisplayed() {
+        composeTestRule.assertDoesNotExistNodeWithTag(LibrariesContentTags.TRY_AGAIN_LAYOUT)
+    }
+
+    fun errorMessageIsDisplayed(errorMessage: String) {
+        composeTestRule.assertTextIsDisplayed(errorMessage)
+    }
+
+}
