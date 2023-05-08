074b921799acaee67df492c760e595882c182dfe
Masoud Fallahpour <masood.fallahpoor@gmail.com>
2023-05-08 16:08:31 +0200

    Prepare the release for version 1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# Version 1.1
+
+- Fix: selected sort order was not persisted
+- Feature: added a button to scroll to the top of the libraries list
+- Improvement: various behind-the-scene refactorings and clean-ups
+
 # Version 1.0
 
 - Eks is born!
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "ir.fallahpoor.eks"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         setProperty("archivesBaseName", "Eks")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
