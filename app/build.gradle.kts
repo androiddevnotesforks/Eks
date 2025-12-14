e5495a0f0897a5039a0678cbb29f70364334f351
Masoud Fallahpour <masood.fallahpoor@gmail.com>
2025-12-14 21:18:56 +0100

    Update AGP, Kotlin and some other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,6 +109,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.runtime.liveData)
 
